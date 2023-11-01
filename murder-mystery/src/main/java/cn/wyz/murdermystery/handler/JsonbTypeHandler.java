3caa1c289a5243b84e5550d1f15f9cdbede45317
wangnanxiang <wangnanxiang@thunisoft.com>
2023-11-01 14:17:45 +0800

    扩充jsonb、set转换器

---
+++ murder-mystery/src/main/java/cn/wyz/murdermystery/handler/JsonbTypeHandler.java
@@ -0,0 +1,69 @@
+package cn.wyz.murdermystery.handler;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.postgresql.util.PGobject;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author wangnanxiang
+ */
+public class JsonbTypeHandler<T extends Object> extends BaseTypeHandler<T> {
+
+    private Class<T> clazz;
+
+    public JsonbTypeHandler(Class<T> clazz) {
+        if (clazz == null) throw new IllegalArgumentException("Type argument cannot be null");
+        this.clazz = clazz;
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException {
+        PGobject jsonObject = new PGobject();
+        jsonObject.setType("jsonb");
+        jsonObject.setValue(this.toJson(parameter));
+        ps.setObject(i, jsonObject);
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return this.toObject(rs.getString(columnName), clazz);
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return this.toObject(rs.getString(columnIndex), clazz);
+    }
+
+    @Override
+    public T getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return this.toObject(cs.getString(columnIndex), clazz);
+    }
+
+    private String toJson(T object) {
+        try {
+            return JSON.toJSONString(object, SerializerFeature.WriteNullListAsEmpty);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private T toObject(String content, Class<?> clazz) {
+        if (content != null && !content.isEmpty()) {
+            try {
+                return (T) JSON.parseObject(content,clazz);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
+    }
+
+}
--- murder-mystery/src/main/java/cn/wyz/murdermystery/handler/SetTypeHandler.java
+++ murder-mystery/src/main/java/cn/wyz/murdermystery/handler/SetTypeHandler.java
@@ -7,19 +7,19 @@ import org.apache.ibatis.type.TypeException;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * @author wnx
  */ // 这个 BaseTypeHandler 里的泛型就是你真实要使用的数组类型，这里写成 List<Object> 能少写几个 typeHandler
-public class ListTypeHandler extends BaseTypeHandler<List<Object>> {
+public class SetTypeHandler extends BaseTypeHandler<Set<Object>> {
 
     /**
      * 这个是 Java 类型往数据库存储时的处理逻辑
      */
     @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, List<Object> parameter, JdbcType jdbcType) throws SQLException {
+    public void setNonNullParameter(PreparedStatement ps, int i, Set<Object> parameter, JdbcType jdbcType) throws SQLException {
         // 如果是空数组的话也给它塞个空数组进去，在 PostgreSQL 中是 {} 这种，应该可以根据业务上的需求调整
         if (parameter.isEmpty()) {
             Connection conn = ps.getConnection();
@@ -30,21 +30,22 @@ public class ListTypeHandler extends BaseTypeHandler<List<Object>> {
         }
 
         String typeName = null;
-        if (parameter.get(0) instanceof Integer) {
+        Object eleType = parameter.iterator().next();
+        if (eleType instanceof Integer) {
             typeName = JDBCType.INTEGER.getName();
-        } else if (parameter.get(0) instanceof String) {
+        } else if (eleType instanceof String) {
             typeName = JDBCType.VARCHAR.getName();
-        } else if (parameter.get(0) instanceof Boolean) {
+        } else if (eleType instanceof Boolean) {
             typeName = JDBCType.BOOLEAN.getName();
-        } else if (parameter.get(0) instanceof Double) {
+        } else if (eleType instanceof Double) {
             typeName = JDBCType.DOUBLE.getName();
-        } else if (parameter.get(0) instanceof Float) {
+        } else if (eleType instanceof Float) {
             typeName = JDBCType.FLOAT.getName();
-        } else if (parameter.get(0) instanceof Long) {
+        } else if (eleType instanceof Long) {
             typeName = JDBCType.BIGINT.getName();
-        } else if (parameter.get(0) instanceof Short) {
+        } else if (eleType instanceof Short) {
             typeName = JDBCType.SMALLINT.getName();
-        } else if (parameter.get(0) instanceof Byte) {
+        } else if (eleType instanceof Byte) {
             typeName = JDBCType.TINYINT.getName();
         }
         if (typeName == null) {
@@ -58,34 +59,34 @@ public class ListTypeHandler extends BaseTypeHandler<List<Object>> {
     }
 
     @Override
-    public List<Object> getNullableResult(ResultSet resultSet, String s) throws SQLException {
-        return getList(resultSet.getArray(s));
+    public Set<Object> getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return getSet(resultSet.getArray(s));
     }
 
     @Override
-    public List<Object> getNullableResult(ResultSet resultSet, int i) throws SQLException {
-        return getList(resultSet.getArray(i));
+    public Set<Object> getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return getSet(resultSet.getArray(i));
     }
 
     @Override
-    public List<Object> getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
-        return getList(callableStatement.getArray(i));
+    public Set<Object> getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return getSet(callableStatement.getArray(i));
     }
 
     /**
      * 上面都是从数据库读取后存入 Java PO 对象时的处理逻辑
      * 这里写了一个统一的方法，因为逻辑都一样
      */
-    private List<Object> getList(Array array) {
+    private Set<Object> getSet(Array array) {
         // 如果数据库字段为 null，这里会转成一个空的 List，根据业务需求更改，赋值为 null 的话，后面对 PO 处理要注意 NPE
         if (array == null) {
-            return Collections.emptyList();
+            return Collections.emptySet();
         }
         try {
-            // 这里要把 java.sql.Array 转成 java.util.List，尝试了多种写法，只有 Copilot 写的不会出警告
+            // 这里要把 java.sql.Array 转成 java.util.Set，尝试了多种写法，只有 Copilot 写的不会出警告
             Object[] objects = (Object[]) array.getArray();
             array.free();
-            return Arrays.stream(objects).collect(Collectors.toList());
+            return Arrays.stream(objects).collect(Collectors.toSet());
         } catch (Exception ignored) {
         }
         return null;
