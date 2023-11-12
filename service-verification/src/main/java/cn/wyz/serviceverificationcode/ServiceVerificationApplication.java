811556f18322da9b9bc4b811420e7a2acad36021
wyz <wyzzovo@gmail.com>
2023-11-12 16:08:32 +0800

    启动类命名

--- service-verification/src/main/java/cn/wyz/serviceverificationcode/ServiceVerificationApplication.java
+++ service-verification/src/main/java/cn/wyz/serviceverificationcode/ServiceVerificationApplication.java
@@ -9,10 +9,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ServiceVerificationCodeApplication {
+public class ServiceVerificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceVerificationCodeApplication.class, args);
+		SpringApplication.run(ServiceVerificationApplication.class, args);
 	}
 
 }
