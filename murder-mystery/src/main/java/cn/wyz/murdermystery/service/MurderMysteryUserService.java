4b282e65a055f3935652a70a3fce3f1b643c323d
zhouzitong <zhouzhitong@coevolution.ai>
2023-12-22 17:19:12 +0800

    fix: 完善游戏用户信息查询, 如果获取用户没有信息是, 就创建用户

--- murder-mystery/src/main/java/cn/wyz/murdermystery/service/MurderMysteryUserService.java
+++ murder-mystery/src/main/java/cn/wyz/murdermystery/service/MurderMysteryUserService.java
@@ -8,6 +8,7 @@ import cn.wyz.murdermystery.bean.dto.MurderMysteryUserDTO;
 import cn.wyz.murdermystery.bean.request.MurderMysteryUserRequest;
 import cn.wyz.murdermystery.type.BlemishDetailType;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -17,10 +18,25 @@ import java.util.List;
  */
 public interface MurderMysteryUserService extends MapperService<MurderMysteryUser, MurderMysteryUserDTO> {
 
+    /**
+     * 根据用户Id 获取游戏用户信息; 如果不存在就新增一个
+     *
+     * @param userId 用户Id
+     * @return 游戏用户信息
+     */
+    @Transactional(rollbackFor = Exception.class)
     default MurderMysteryUserDTO getByUserId(Long userId) {
         MurderMysteryUserRequest req = new MurderMysteryUserRequest();
         req.setUserId(userId);
         MurderMysteryUserDTO mmUser = getOne(req);
+        if (mmUser == null) {
+            mmUser = new MurderMysteryUserDTO();
+            mmUser.setUserId(userId);
+            mmUser.setGrade(1);
+            mmUser.setCount(0);
+            mmUser.setBlemishCount(0);
+            mmUser = this.add(mmUser);
+        }
         return mmUser;
     }
 
