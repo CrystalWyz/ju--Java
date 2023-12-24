11742e5d0ae3ac6074ce3fd5a7cff8604284b6a5
jouzitong <528382226@qq.com>
2023-12-24 16:40:40 +0800

    feat: 剧本杀DTO, 增加两个get方法

--- murder-mystery/src/main/java/cn/wyz/murdermystery/bean/dto/MurderMysteryDTO.java
+++ murder-mystery/src/main/java/cn/wyz/murdermystery/bean/dto/MurderMysteryDTO.java
@@ -26,71 +26,71 @@ import java.util.Objects;
 @Schema(name = "MurderMysteryDTO", description = "聚--剧本杀dto")
 public class MurderMysteryDTO extends BaseDTO {
 
-    @Schema(name = "title",description = "标题")
+    @Schema(name = "title", description = "标题")
     private String title;
 
     @JSONField(format = "yyyy-MM-dd HH:mm")
-    @Schema(name = "beginExpected",description = "预计开始时间")
+    @Schema(name = "beginExpected", description = "预计开始时间")
     private LocalDateTime beginExpected;
 
     @JSONField(format = "yyyy-MM-dd HH:mm")
-    @Schema(name = "finishExpected",description = "预计结束时间")
+    @Schema(name = "finishExpected", description = "预计结束时间")
     private LocalDateTime finishExpected;
 
     @JSONField(format = "yyyy-MM-dd HH:mm")
-    @Schema(name = "beginActual",description = "实际开始时间")
+    @Schema(name = "beginActual", description = "实际开始时间")
     private LocalDateTime beginActual;
 
     @JSONField(format = "yyyy-MM-dd HH:mm")
-    @Schema(name = "finishActual",description = "实际结束时间")
+    @Schema(name = "finishActual", description = "实际结束时间")
     private LocalDateTime finishActual;
 
-    @Schema(name = "description",description = "聚描述信息")
+    @Schema(name = "description", description = "聚描述信息")
     private String description;
 
     /**
      * 游戏状态
      */
-    @Schema(name = "status",description = "游戏状态")
+    @Schema(name = "status", description = "游戏状态")
     private GameStatus status;
 
-    @Schema(name = "scale",description = "规模")
+    @Schema(name = "scale", description = "规模")
     private Integer scale;
 
-    @Schema(name = "girlParticipantNum",description = "需要女生参与人数")
+    @Schema(name = "girlParticipantNum", description = "需要女生参与人数")
     private Integer girlParticipantNum;
 
-    @Schema(name = "boyParticipantNum",description = "需要男生参与人数")
+    @Schema(name = "boyParticipantNum", description = "需要男生参与人数")
     private Integer boyParticipantNum;
 
     /**
      * @see MurderConfigConstance MurderConfigConstance
      */
-    @Schema(name = "config",description = "配置信息")
+    @Schema(name = "config", description = "配置信息")
     private JSONObject config;
 
-    @Schema(name = "girlParticipant",description = "女生参与列表")
+    @Schema(name = "girlParticipant", description = "女生参与列表")
     private List<Long> girlParticipant = Lists.newArrayList();
 
-    @Schema(name = "boyParticipant",description = "男生参与列表")
+    @Schema(name = "boyParticipant", description = "男生参与列表")
     private List<Long> boyParticipant = Lists.newArrayList();
 
-    @Schema(name = "applyParticipant",description = "申请参与列表")
+    @Schema(name = "applyParticipant", description = "申请参与列表")
     private List<Long> applyParticipant = Lists.newArrayList();
 
-    @Schema(name = "signInParticipant",description = "签到列表")
+    @Schema(name = "signInParticipant", description = "签到列表")
     private List<Long> signInParticipant = Lists.newArrayList();
 
-    @Schema(name = "area",description = "所属地区")
+    @Schema(name = "area", description = "所属地区")
     private List<Integer> area;
 
-    @Schema(name = "address",description = "详细地址")
+    @Schema(name = "address", description = "详细地址")
     private String address;
 
-    @Schema(name = "shopName",description = "店铺名")
+    @Schema(name = "shopName", description = "店铺名")
     private String shopName;
 
-    @Schema(name = "tags",description = "标签")
+    @Schema(name = "tags", description = "标签")
     private List<Long> tags = Lists.newArrayList();
 
     /**
@@ -123,4 +123,14 @@ public class MurderMysteryDTO extends BaseDTO {
     public boolean isBoyFull() {
         return Objects.equals(this.getBoyParticipant().size(), this.getBoyParticipantNum());
     }
+
+
+    public int getBoyNum() {
+        return getBoyParticipant().size();
+    }
+
+    public int getGirlNum() {
+        return getGirlParticipant().size();
+    }
+
 }
